118d19ed427bbb1e91c3cf916ccf83a66cc5bfc8
Chan-Ho Suh <chanhosuh@gmail.com>
2016-10-16 16:09:53 -0400

    modify theme settings

--- pelicanconf.py
+++ pelicanconf.py
@@ -33,3 +33,27 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+THEME = 'blue-penguin'
+
+# provided as examples, they make ‘clean’ urls. used by MENU_INTERNAL_PAGES.
+TAGS_URL           = 'tags'
+TAGS_SAVE_AS       = 'tags/index.html'
+AUTHORS_URL        = 'authors'
+AUTHORS_SAVE_AS    = 'authors/index.html'
+CATEGORIES_URL     = 'categories'
+CATEGORIES_SAVE_AS = 'categories/index.html'
+ARCHIVES_URL       = 'archives'
+ARCHIVES_SAVE_AS   = 'archives/index.html'
+
+# use those if you want pelican standard pages to appear in your menu
+MENU_INTERNAL_PAGES = (
+#     ('Tags', TAGS_URL, TAGS_SAVE_AS),
+#     ('Authors', AUTHORS_URL, AUTHORS_SAVE_AS),
+#     ('Categories', CATEGORIES_URL, CATEGORIES_SAVE_AS),
+    ('Archives', ARCHIVES_URL, ARCHIVES_SAVE_AS),
+)
+# additional menu items
+MENUITEMS = (
+#     ('Github', 'https://github.com/'),
+)
