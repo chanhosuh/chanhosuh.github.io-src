a7b12964c212809e5ef3f360fa60c58b8af22cba
Chan-Ho Suh <chanhosuh@gmail.com>
2017-04-30 17:54:45 -0400

    add about me page to menu

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,6 +12,8 @@ TIMEZONE = 'America/New_York'
 
 DEFAULT_LANG = u'en'
 
+DISPLAY_PAGES_ON_MENU = True
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
