e52e90c633c3210a61610c84a47f51171ff77125
Chan-Ho Suh <chanhosuh@gmail.com>
2019-07-02 22:48:42 -0400

    fix SITEURL to be https

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://chanhosuh.github.io'
+SITEURL = 'https://chanhosuh.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
